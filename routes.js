35d4455dd8aa4c1cffad2fed5f1b91712bf577ef
Tsvetomir Nikolov <tsvetomir.nikolov@mentormate.com>
2018-09-12 19:59:42 +0300

    Changed weight calculations

--- routes.js
+++ routes.js
@@ -42,16 +42,47 @@ module.exports = (app) => {
      * GET STL file details
      */
     router.get("/file/:name/details", function (req, res) {
-        const calculatePrice = (weight) => {
-            return (0.4 + (weight * 0.2)).toFixed(2);
+        const calculateWeight = (volume, area) => {
+            // Weight is volume * density of material.
+            // It looks like the calculation is using a density of 1.04.
+            // PLA has a density of 1.25 g/cm^3.
+            // ABS has a density of 1.06 g/cm^3.
+
+            const PLA_DENSITY = 1.25; // g/cm3
+            const LAYER_SIZE = 0.1; // cm
+            const FILL_DENSITY = 0.2;
+
+            let highDenceVolume = area * LAYER_SIZE; // cm3
+            let lowDenceVolume = volume - highDenceVolume; // cm3
+
+            // HighDenceVolume is not correct because the layer in the botder is calculated in the twice (in the horizontal and vertical surfaceс)
+            // For higher LAYER_SIZE will increase the error rate
+            // Also the of high dencity layer is too thick
+            // Sooo... it will charge more for complex models 👿
+            if (lowDenceVolume < 0) {
+                lowDenceVolume = 0;
+            }
+
+            console.log('area', area, 'g/cm3');
+            console.log('totalVolume', volume);
+            console.log('highDenceVolume', highDenceVolume);
+            console.log('lowDenceVolume', lowDenceVolume);
+
+            const weight = (lowDenceVolume * PLA_DENSITY * FILL_DENSITY) + (highDenceVolume * PLA_DENSITY);
+            return weight;
+        }
+
+        const calculatePricePerWeight = (weight) => {
+            const FIXED_PRICE = 0.4;
+            const PRICE_PER_GRAM = 0.2;
+            return (FIXED_PRICE + (weight * PRICE_PER_GRAM)).toFixed(2);
         };
 
         const NodeStl = require("node-stl");
         var filePath = __dirname + `/uploads/${req.params.name}`;
         var stl = NodeStl(filePath);
-        const PLAdensity = 1.25;
-        const weight = stl.volume * PLAdensity;
-        const price = calculatePrice(stl.volume * PLAdensity);
+        const weight = calculateWeight(stl.volume, stl.area / 100);
+        const price = calculatePricePerWeight(weight);
 
         res.json({
             volume: stl.volume, // cm^3
@@ -63,10 +94,5 @@ module.exports = (app) => {
                 currency: 'BGN'
             }
         });
-
-        // Weight is volume * density of material.
-        // It looks like the calculation is using a density of 1.04.
-        // PLA has a density of 1.25 g/cm^3.
-        // ABS has a density of 1.06 g/cm^3.
     });
 }
